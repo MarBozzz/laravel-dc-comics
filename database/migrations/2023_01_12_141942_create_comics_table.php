98ea38a25e591e1be1700dfada32a531bde4c238
MarBozzz <mbozzuto@libero.it>
2023-01-12 16:23:28 +0100

    [added] model, controller, db, migration, seeding

---
+++ database/migrations/2023_01_12_141942_create_comics_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comics', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 100);
+            $table->string('slug', 110)->unique();
+            $table->text('description')->nullable();
+            $table->string('thumb')->nullable();
+            $table->string('price');
+            $table->string('series')->nullable();
+            $table->string('sale_date', 10);
+            $table->string('type', 30)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comics');
+    }
+};
